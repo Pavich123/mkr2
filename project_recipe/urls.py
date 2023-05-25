9ccce49a5a9276a6b6bec2e8f2d63b7c94a5f53e
Pavich123 <pavlyuchenko0209@gmail.com>
2023-05-25 10:36:42 +0200

    final commit

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from recipe import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.last_views, name='main'),
+    path('all/', views.all_categories, name='category_list'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,21 @@
-#new view
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Category,Recipe
+
+
+def last_views(request):
+    recipes = Recipe.objects.order_by('-created_at')[:5]
+    my_context={'recipes': recipes}
+    return render(request, 'main.html', my_context)
+
+
+def all_categories(request):
+    categories = Category.objects.all()
+
+    counts = []
+    for category in categories:
+        counts.append({
+            'name_of_category': category.name,
+            'count_of_every': category.categories.count()
+        })
+    return render(request, 'new.html', {'counts': counts})
